85281edb7eb9b180b8c70ccf75ad704e7aec3744
skikda <kiripeng@outlook.com>
2021-05-06 19:28:25 +0800

    修改作业的不足

--- week2/dao/dao.go
+++ week2/dao/dao.go
@@ -7,6 +7,16 @@ import (
 
 //GetCount
 //
+// 按照业务性来做就是需要把code.ErrNoRows来作为ddd业务
+//if err == sql.ErrNoRows {
+//
+//return errors.Wrapf(code.NotFound, fmt.Sprintf("sql: %s error: %v", sql, err))
+//
+//} else {
+//
+//return errors.Wrapf(code.Internal, fmt.Sprintf("sql: %s error: %v", sql, err))
+//
+//}
 func GetCount() (int64, error) {
 	var count int64
 	//TODO: 执行了查询
